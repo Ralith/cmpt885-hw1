993896e557b1b58522b1f29c709d5f5b6c730918
Benjamin Saunders <besaunde@sfu.ca>
2011-06-15 18:55:18 -0700

    taskset is a heisentroll.

--- gatherData.sh
+++ gatherData.sh
@@ -4,8 +4,6 @@ MAXTHREADS=24
 SUM=1000000
 INCREMENT=2
 ITERATIONS=5
-CPUS=4
-CORES=6
 
 if [ `uname` = 'Darwin' ]; then
     THRCOUNTER="jot - 1 $MAXTHREADS $INCREMENT"
@@ -15,14 +13,11 @@ else
     ITERCOUNTER="seq 1 $ITERATIONS"
 fi
 
-ALLCORES="`seq -s , 0 $CPUS $[$CPUS*$CORES-1]`,`seq -s , 1 $CPUS $[$CPUS*$CORES-1]`,`seq -s , 2 $CPUS $[$CPUS*$CORES-1]`,`seq -s , 3 $CPUS $[$CPUS*$CORES-1]`"
-
 echo -e "Prog\tIter\tThreads\tTime"
 for PROG in bin/counter/*; do
     for THREADS in `$THRCOUNTER`; do
         for ITERATION in `$ITERCOUNTER`; do
-	    COREIDS=`echo $ALLCORES |cut -d , -f -$THREADS`
-	    read RESULT TIME <<< `taskset -c $COREIDS $PROG -t $THREADS -i $SUM |cut -d : -f 2`
+	    read RESULT TIME <<< `$PROG -t $THREADS -i $SUM |cut -d : -f 2`
 	    echo -e "`basename $PROG`\t${ITERATION}\t${THREADS}\t${TIME}"
         done
     done
