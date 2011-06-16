1f260540e777061a180c59e8dae723eb9c738e01
Benjamin Saunders <besaunde@sfu.ca>
2011-06-15 17:21:03 -0700

    Working MCS lock impl.

--- src/locks/mcsLock.hpp
+++ src/locks/mcsLock.hpp
@@ -1,19 +1,47 @@
-#include "../atomic_ops.h"
+#include "../padded_type.hpp"
+#include "../ryatomic.h"
 
-class Lock {
-private:
-	volatile mcs_qnode_t *queue;
+#define MAX_THREADS 64
+
+class Lock
+{
+        padded_type::ptype<int>
+                wait_pos,
+                lock_pos,
+                spinners[MAX_THREADS]
+        ;
+
 public:
-	Lock() : queue(0) {}
-	void lock() {
-		mcs_qnode_t *thread_node = new mcs_qnode_t;
-		mcs_acquire(&queue, thread_node);
-	}
-	void unlock() {
-		mcs_qnode_t *thread_node;
-		while(thread_node->next) {
-			thread_node = thread_node->next;
-		}
-		mcs_release(&queue, thread_node);
-	}
+        Lock()
+        {
+                wait_pos.value=-1;
+                lock_pos.value=0;
+
+                /* initialize the structure as [T][F]...[F] */
+                spinners[0].value=true;
+                for(u_int8_t i=1;i<MAX_THREADS;i++) spinners[i].value=false;
+        }
+
+        inline void lock()
+        {
+                /* increment and return spin position */
+                int local_wait_pos=atomic_inc(&wait_pos.value)%MAX_THREADS;
+                if(local_wait_pos<0) local_wait_pos+=MAX_THREADS;
+
+                /* spin */
+                while(!spinners[local_wait_pos].value);
+
+                /* free spin position */
+                spinners[local_wait_pos].value=false;
+        }
+
+        inline void unlock()
+        {
+                /* increment global pointer */
+                lock_pos.value=(lock_pos.value+1)%MAX_THREADS;
+
+                /* "signal" the thread waiting on lock_pos */
+                spinners[lock_pos.value].value=true;
+        }
 };
+
