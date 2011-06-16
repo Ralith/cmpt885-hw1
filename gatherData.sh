8116aa1147232a4feedd01425fa4a23b969979b3
Benjamin Saunders <besaunde@sfu.ca>
2011-06-15 18:13:13 -0700

    Portability fixes.

--- gatherData.sh
+++ gatherData.sh
@@ -2,18 +2,21 @@
 
 MAXTHREADS=24
 SUM=1000000
+INCREMENT=3
 ITERATIONS=5
 
 if [ `uname` = 'Darwin' ]; then
-    COUNTER='jot -'
+    THRCOUNTER="jot - 1 $MAXTHREADS $INCREMENT"
+    ITERCOUNTER="jot - 1 $ITERATIONS"
 else
-    COUNTER='seq'
+    THRCOUNTER="seq 1 $INCREMENT $MAXTHREADS"
+    ITERCOUNTER="jot - 1 $ITERATIONS"
 fi
 
 echo -e "Prog\tIter\tThreads\tTime"
 for PROG in bin/counter/*; do
-    for THREADS in `$COUNTER 1 $MAXTHREADS`; do
-        for ITERATION in `$COUNTER 1 $ITERATIONS`; do
+    for THREADS in `$THRCOUNTER`; do
+        for ITERATION in `$ITERCOUNTER`; do
 	    read RESULT TIME <<< `$PROG -t $THREADS -i $SUM |cut -d : -f 2`
 	    echo -e "`basename $PROG`\t${ITERATION}\t${THREADS}\t${TIME}"
         done
