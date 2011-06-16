f44b405a2517ddf6d41a28ffebbcab553d9b19f3
Benjamin Saunders <besaunde@sfu.ca>
2011-06-15 18:29:15 -0700

    Add core binding.

--- gatherData.sh
+++ gatherData.sh
@@ -4,6 +4,8 @@ MAXTHREADS=24
 SUM=1000000
 INCREMENT=3
 ITERATIONS=5
+CPUS=4
+CORES=6
 
 if [ `uname` = 'Darwin' ]; then
     THRCOUNTER="jot - 1 $MAXTHREADS $INCREMENT"
@@ -13,11 +15,14 @@ else
     ITERCOUNTER="jot - 1 $ITERATIONS"
 fi
 
+ALLCORES="`seq -s , 0 $CPUS $[$CPUS*$CORES-1]`,`seq -s , 1 $CPUS $[$CPUS*$CORES-1]`,`seq -s , 2 $CPUS $[$CPUS*$CORES-1]`,`seq -s , 3 $CPUS $[$CPUS*$CORES-1]`"
+
 echo -e "Prog\tIter\tThreads\tTime"
 for PROG in bin/counter/*; do
     for THREADS in `$THRCOUNTER`; do
         for ITERATION in `$ITERCOUNTER`; do
-	    read RESULT TIME <<< `$PROG -t $THREADS -i $SUM |cut -d : -f 2`
+	    COREIDS=`echo $ALLCORES |cut -d , -f -$THREADS`
+	    read RESULT TIME <<< `taskset -c $COREIDS $CORES $PROG -t $THREADS -i $SUM |cut -d : -f 2`
 	    echo -e "`basename $PROG`\t${ITERATION}\t${THREADS}\t${TIME}"
         done
     done
